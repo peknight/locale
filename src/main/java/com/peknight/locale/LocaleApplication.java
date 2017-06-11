3144dec14c4385bed4d931de6a7844f5627ebbbe
peknight <peknight@qq.com>
2017-06-11 09:03:01 +0800

    Init Repository

---
+++ src/main/java/com/peknight/locale/LocaleApplication.java
@@ -0,0 +1,12 @@
+package com.peknight.locale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocaleApplication.class, args);
+	}
+}
